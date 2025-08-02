f4f045f350107be633aeb60405b867e4540658b6
plowsters <brandonplowman6503@gmail.com>
2025-08-02 16:27:59 -0700

    Created base activity_login.xml with imported drawables from Figma Mockup

--- app/src/main/java/com/example/pocketgrimoire/LoginActivity.java
+++ app/src/main/java/com/example/pocketgrimoire/LoginActivity.java
@@ -3,18 +3,19 @@ package com.example.pocketgrimoire;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.pocketgrimoire.databinding.ActivityMainBinding;
+
+import com.example.pocketgrimoire.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivityLoginBinding binding;
 
     public static final String TAG = "PocketGrimoireDB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = com.example.pocketgrimoire.databinding.ActivityMainBinding.inflate(getLayoutInflater());
+        binding = com.example.pocketgrimoire.databinding.ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
