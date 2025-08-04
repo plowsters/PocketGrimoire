eba4f07c974f4cd147bc5767e7c834161716418d
Serena Ngo <sngo@csumb.edu>
2025-08-03 22:05:17 -0700

    Created ItemsDAO and updated the database

---
+++ app/src/main/java/com/example/pocketgrimoire/database/ItemsDAO.java
@@ -0,0 +1,27 @@
+package com.example.pocketgrimoire.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.pocketgrimoire.database.entities.Items;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+
+@Dao
+public interface ItemsDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable insert(Items items);
+
+    @Query("SELECT * FROM " + PocketGrimoireDatabase.ITEMS_TABLE + " ORDER BY itemID") //static reference to the name of the table that is defined in the database
+
+    Flowable<List<Items>> getAllItems();
+
+    @Query("SELECT COUNT(*) FROM " + PocketGrimoireDatabase.ITEMS_TABLE)
+    Flowable<Integer> itemsCount();
+}
