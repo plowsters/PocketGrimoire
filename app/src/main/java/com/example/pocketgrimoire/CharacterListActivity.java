23d3f77f4e2fcd352042d8a40925863d05e8a8ef
Serena Ngo <sngo@csumb.edu>
2025-08-07 02:20:58 -0700

    Created CharacterListAdapter and CharacterListViewHolder. Added a RxJava implementation in build.gradle for AndroidSchedulers.

--- app/src/main/java/com/example/pocketgrimoire/CharacterListActivity.java
+++ app/src/main/java/com/example/pocketgrimoire/CharacterListActivity.java
@@ -1,8 +1,10 @@
 package com.example.pocketgrimoire;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,6 +13,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pocketgrimoire.database.PocketGrimoireRepository;
 import com.example.pocketgrimoire.databinding.ActivityCharacterListBinding;
+import com.example.pocketgrimoire.viewmodel.CharacterListAdapter;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class CharacterListActivity extends AppCompatActivity {
 
@@ -19,34 +25,41 @@ public class CharacterListActivity extends AppCompatActivity {
     private static final String CHARACTER_LIST_ACTIVITY_USER_ID = "CHARACTER_LIST_ACTIVITY_USER_ID";
     private PocketGrimoireRepository repository;
     private int userID;
+    public static final int LOGGED_OUT = -1;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        binding = ActivityCharacterListBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//        userID = getIntent().getIntExtra(CHARACTER_LIST_ACTIVITY_USER_ID,LOGGED_OUT);
-//
-//
-//        characterListViewModel = new ViewModelProvider(this).get(com.example.pocketgrimoire.database.viewHolders.CharacterListViewModel.class);
-//
-//        //display list of characters
-//        RecyclerView recyclerView = binding.characterListDisplayRecyclerView;
-//        final CharacterListAdapter adapter = new CharacterListAdapter(new CharacterListAdapter.CharacterSheetDiff());
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        repository = PocketGrimoireRepository.getRepository(getApplication());
-//
-//        //check for characters under UserID then adds their data
-//        characterListViewModel.getAllCharacterSheetByUserId(userID).observe(this, characterList -> {
-//            adapter.submitList(characterList); //updates list
-//        });
-//    }
-//    public static Intent characterListIntentFactory(Context context, int userID) {
-//        Intent intent = new Intent(context, CharacterListActivity.class);
-//        intent.putExtra(CHARACTER_LIST_ACTIVITY_USER_ID, userID);
-//        return intent;
-//    }
+    @SuppressLint("CheckResult")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCharacterListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        userID = getIntent().getIntExtra(CHARACTER_LIST_ACTIVITY_USER_ID,LOGGED_OUT);
+
+
+        characterListViewModel = new ViewModelProvider(this).get(com.example.pocketgrimoire.database.viewHolders.CharacterListViewModel.class);
+
+        //display list of characters
+        RecyclerView recyclerView = binding.characterListDisplayRecyclerView;
+        final CharacterListAdapter adapter = new CharacterListAdapter(new CharacterListAdapter.CharacterSheetDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        repository = PocketGrimoireRepository.getRepository(getApplication()).blockingGet();
+
+        //check for characters under UserID then adds their data
+        characterListViewModel.getAllCharacterSheetByUserId(userID)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(characterList -> {
+                    adapter.submitList(characterList);
+                }, throwable -> {
+                    Log.e("RX", "Error loading characters", throwable);
+                });
+    }
+    public static Intent characterListIntentFactory(Context context, int userID) {
+        Intent intent = new Intent(context, CharacterListActivity.class);
+        intent.putExtra(CHARACTER_LIST_ACTIVITY_USER_ID, userID);
+        return intent;
+    }
 
 }
