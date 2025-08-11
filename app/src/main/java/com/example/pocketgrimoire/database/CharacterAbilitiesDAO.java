7729002df30789a59061ce5a8ab7359b5ada30b7
Dannyj21 <161805434+Dannyj21@users.noreply.github.com>
2025-08-11 12:37:00 -0700

    Update CharacterAbilitiesDAO.java

--- app/src/main/java/com/example/pocketgrimoire/database/CharacterAbilitiesDAO.java
+++ app/src/main/java/com/example/pocketgrimoire/database/CharacterAbilitiesDAO.java
@@ -15,17 +15,17 @@ import io.reactivex.rxjava3.core.Flowable;
 
 @Dao
 public interface CharacterAbilitiesDAO {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     Completable insert(CharacterAbilities ca);
 
     @Update
     Completable update(CharacterAbilities ca);
 
-    @Query("SELECT * FROM " + PocketGrimoireDatabase.CHARACTER_ABILITIES_TABLE + " WHERE characterId = :characterId")
+    @Query("SELECT * FROM " + PocketGrimoireDatabase.CHARACTER_ABILITIES_TABLE + " WHERE characterID = :characterId")
     Flowable<List<CharacterAbilities>> getByCharacterId(int characterId);
 
-    @Query("DELETE FROM " + PocketGrimoireDatabase.CHARACTER_ABILITIES_TABLE + " WHERE characterId = :characterId")
+    @Query("DELETE FROM " + PocketGrimoireDatabase.CHARACTER_ABILITIES_TABLE + " WHERE characterID = :characterId")
     Completable clearForCharacter(int characterId);
-
 }
 
